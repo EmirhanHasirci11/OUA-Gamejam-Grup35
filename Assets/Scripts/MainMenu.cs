34794c7976716805cdcb234a0696bab2c5497f17
Emirhan Hasirci <emir_han886@outlook.com>
2024-03-16 16:04:55 +0300

    Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Start is called before the first frame update    
+    public GameObject MenuBtn;
+    public GameObject BackBtn;
+    public GameObject CreditsCanvas;
+    public Image Dark;       
+    public void Play()
     {
-        
+        StartCoroutine(Iterate());
+    }
+    IEnumerator Iterate()
+    {
+        Dark.gameObject.SetActive(true);
+        StartCoroutine(FadeIn(Dark, 2));
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Credits()
+    {
+        MenuBtn.SetActive(false);
+        BackBtn.SetActive(true);
+        CreditsCanvas.SetActive(true);
+    }
+    public void Back()
+    {           
+        MenuBtn.SetActive(true);
+        BackBtn.SetActive(false);
+        CreditsCanvas.SetActive(false);              
+    }   
+    public void Options()
+    {
+        MenuBtn.SetActive(false);        
+        BackBtn.SetActive(true);
+    }
+    private YieldInstruction Instruction = new YieldInstruction();
+    public IEnumerator FadeOut(Image image, float time)
+    {//general fade out effect for images
+        float elapsedTime = 0.0f;
+        Color c = image.color;
+        while (elapsedTime < time)
+        {
+            yield return Instruction;
+            elapsedTime += Time.deltaTime;
+            c.a = 1.0f - Mathf.Clamp01(elapsedTime / time);
+            image.color = c;
+        }
     }
 
-   public void CloseTheGame()
-    {        
-        Application.Quit();
+    public IEnumerator FadeIn(Image image, float time)
+    {//general fade in effect for images
+        float elapsedTime = 0.0f;
+        Color c = image.color;
+        while (elapsedTime < time)
+        {
+            yield return Instruction;
+            elapsedTime += Time.deltaTime;
+            c.a = Mathf.Clamp01(elapsedTime / time);
+            image.color = c;
+        }
     }
 }
